cdaec9ddc400d0102b115d68d95613a480eb75d5
ohtomi <ohtomi.kenichi@gmail.com>
2019-01-17 22:12:28 +0900

    Add test for <Link/> component clicked

--- src/Link.test.js
+++ src/Link.test.js
@@ -3,10 +3,52 @@ import ReactTestRenderer from 'react-test-renderer'
 
 import Link from './Link'
 
-it('renders Link', () => {
+it('renders Link with replace, then click', () => {
+    const props = {
+        replaceHistory: jest.fn(),
+        pushHistory: jest.fn(),
+        replace: true,
+        to: '/abc'
+    }
     const renderer = ReactTestRenderer.create(
-        <Link to={'/abc'}/>
+        <Link {...props}/>
     )
+
     const instance = renderer.root
     expect(instance.findByType(Link).props.to).toBe('/abc')
+
+    const ev = {
+        preventDefault: jest.fn()
+    }
+    let tree = renderer.toJSON()
+    tree.props.onClick(ev)
+
+    expect(props.replaceHistory).toHaveBeenCalled()
+    expect(props.pushHistory).not.toHaveBeenCalled()
+    expect(ev.preventDefault).toHaveBeenCalled()
+})
+
+it('renders Link without replace, then click', () => {
+    const props = {
+        replaceHistory: jest.fn(),
+        pushHistory: jest.fn(),
+        replace: false,
+        to: '/abc'
+    }
+    const renderer = ReactTestRenderer.create(
+        <Link {...props}/>
+    )
+
+    const instance = renderer.root
+    expect(instance.findByType(Link).props.to).toBe('/abc')
+
+    const ev = {
+        preventDefault: jest.fn()
+    }
+    let tree = renderer.toJSON()
+    tree.props.onClick(ev)
+
+    expect(props.replaceHistory).not.toHaveBeenCalled()
+    expect(props.pushHistory).toHaveBeenCalled()
+    expect(ev.preventDefault).toHaveBeenCalled()
 })
