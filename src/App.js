a69aae28b70af11edc1e94383d8b9a02fba18af9
ohtomi <ohtomi.kenichi@gmail.com>
2019-01-29 07:34:57 +0900

    Fix routing actions binding problem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {renderRoutes} from 'react-router-config'
 
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import * as routing from './containers/routing/reducer'
+import * as routing from './containers/routing/actions'
 
 import logo from './logo.svg'
 import './App.css'
