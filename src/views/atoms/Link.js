3dc5f5ddb5bb95b9f7467a58603fde2906b36f9e
ohtomi <ohtomi.kenichi@gmail.com>
2019-01-31 21:22:55 +0900

    Add molecule component

--- src/views/atoms/Link.js
+++ src/views/atoms/Link.js
@@ -1,17 +1,33 @@
+// @flow
+
 import React from 'react'
 
-const Link = ({replaceHistory, pushHistory, replace, dispatch, to, children}) => {
+export type LinkProps = {
+    replace?: boolean,
+    to: string,
+    label: string
+}
+
+type Props = {
+    replaceHistory?: Function,
+    pushHistory?: Function,
+    replace?: boolean,
+    to: string,
+    children?: any
+}
+
+const Link = (props: Props) => {
     const onClick = (ev) => {
         ev.preventDefault()
-        if (replace) {
-            replaceHistory(to)
-        } else {
-            pushHistory(to)
+        if (props.replace && props.replaceHistory) {
+            props.replaceHistory(props.to)
+        } else if (props.pushHistory) {
+            props.pushHistory(props.to)
         }
     }
     return (
         <button onClick={onClick}>
-            {children}
+            {props.children}
         </button>
     )
 }
--- src/views/pages/Child.js
+++ src/views/pages/Child.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import {renderRoutes} from 'react-router-config'
 
-import Link from '../atoms/Link'
+import LinkList from '../molecules/LinkList'
 import BasicLayout from '../templates/BasicLayout'
 
 const Child = ({route, match: {params: {id}}, actions}) => {
+    const links = [
+        {to: '/', label: 'Home'}
+    ]
     return (
         <BasicLayout>
             <div style={{border: '1px solid', padding: '5px', margin: '3px'}}>
                 Child {id}
                 {renderRoutes(route.routes)}
-                <div>
-                    <Link to={'/'} {...actions.routing}>Home</Link>
-                </div>
+                <LinkList links={links} actions={actions}/>
             </div>
         </BasicLayout>
     )
