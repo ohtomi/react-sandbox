44d6fdb8fa973e4b47c66d9fbd60a76268b00f11
ohtomi <ohtomi.kenichi@gmail.com>
2019-02-23 20:12:46 +0900

    Pass locale name from store to IntlProvider when rendering App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import {renderRoutes} from 'react-router-config'
 
+import {IntlProvider} from 'react-intl'
+import translations from './i18n/locales'
+
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
+import * as locale from './containers/locale/actions'
 import * as routing from './containers/routing/actions'
 import * as counter from './containers/counter/actions'
 
 import './App.css'
 
 const App = ({route, state, actions}) => {
+    const localeName = state.locale.name
+    const messages = translations[localeName]
+
     return (
-        <div className="App">
-            {renderRoutes(route.routes, {state, actions})}
-        </div>
+        <IntlProvider locale={localeName} key={localeName} messages={messages}>
+            <div className="App">
+                {renderRoutes(route.routes, {state, actions})}
+            </div>
+        </IntlProvider>
     )
 }
 
@@ -27,6 +36,7 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         actions: {
+            locale: bindActionCreators(locale, dispatch),
             routing: bindActionCreators(routing, dispatch),
             counter: bindActionCreators(counter, dispatch)
         }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import {HashRouter} from 'react-router-dom'
 import {renderRoutes} from 'react-router-config'
 import history from './history'
 
-import {addLocaleData, IntlProvider} from 'react-intl'
+import {addLocaleData} from 'react-intl'
+import enLocaleData from 'react-intl/locale-data/en'
 import jaLocaleData from 'react-intl/locale-data/ja'
-import translations from './i18n/locales'
 
 import {Provider} from 'react-redux'
 import store from './store'
@@ -17,20 +17,16 @@ import './index.css'
 
 import * as serviceWorker from './serviceWorker'
 
+addLocaleData(enLocaleData)
 addLocaleData(jaLocaleData)
-
-const locale = store.getState().locale.name
-const messages = translations[locale]
 
 const renderer = (location) => {
     ReactDOM.render(
-        <IntlProvider locale={locale} key={locale} messages={messages}>
-            <Provider store={store}>
-                <HashRouter>
-                    {renderRoutes(routes)}
-                </HashRouter>
-            </Provider>
-        </IntlProvider>,
+        <Provider store={store}>
+            <HashRouter>
+                {renderRoutes(routes)}
+            </HashRouter>
+        </Provider>,
         document.getElementById('root')
     )
 }
