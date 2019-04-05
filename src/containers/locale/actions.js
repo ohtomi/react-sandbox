649aa7c22e35c28cd2f42673d5384de91b9b6c97
ohtomi <ohtomi.kenichi@gmail.com>
2019-04-05 23:11:44 +0900

    Add typescript

--- src/containers/locale/actions.js
+++
@@ -1,8 +0,0 @@
-import * as ActionType from '../../actions'
-
-export const switchLocale = (locale) => {
-    return {
-        type: ActionType.SWITCH_LOCALE,
-        payload: {locale}
-    }
-}
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux'
-
-import reducers from './reducers'
-import preloadedState from './preloadedState'
-import enhancer from './enhancer'
-
-const store = createStore(reducers, preloadedState, enhancer)
-
-export default store
